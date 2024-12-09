2964edeba8b993a37511054169716f1688159ef5
Blert Osmani <blertosmani15@gmail.com>
2024-12-09 13:12:27 +0100

    Updated the Tasks model. Removed the assignee and reporter field.

--- tasks/models.py
+++ tasks/models.py
@@ -25,8 +25,6 @@ class Task(BaseModel):
     priority = models.CharField(max_length=6, choices=Priority.choices, default=Priority.NORMAL)
     due_date = models.DateTimeField(null=True, blank=True)
     completed_at = models.DateTimeField(null=True, blank=True)
-    assignee = models.ForeignKey(User, null=True, on_delete=models.SET_NULL, related_name="assignee_tasks")
-    reporter = models.ForeignKey(User, null=True, on_delete=models.SET_NULL, related_name="reporter_tasks")
     created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name="created_by_tasks")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
